991bc4f102ff1a8752df562d4daab38c4443cafd
poojithairosha <poojithairosha9311@gmail.com>
2023-05-13 20:14:46 +0530

    Extract sensitive data to application.properties file

--- src/main/java/com/ceyloneseenvoy/ceyloneseenvoy/util/HibernateUtil.java
+++ src/main/java/com/ceyloneseenvoy/ceyloneseenvoy/util/HibernateUtil.java
@@ -8,8 +8,18 @@ public class HibernateUtil {
     private static SessionFactory SESSION_FACTORY;
 
     public static SessionFactory getSessionFactory() {
-        if (SESSION_FACTORY == null)
-            SESSION_FACTORY = new Configuration().configure().buildSessionFactory();
+        if (SESSION_FACTORY == null) {
+            PropertiesUtil instance = PropertiesUtil.getInstance();
+
+            Configuration configuration = new Configuration();
+            configuration.setProperty("hibernate.connection.url", instance.getProperty("db.url"));
+            configuration.setProperty("hibernate.connection.username", instance.getProperty("db.username"));
+            configuration.setProperty("hibernate.connection.password", instance.getProperty("db.password"));
+            configuration.setProperty("hibernate.connection.driver_class", instance.getProperty("db.driver"));
+            configuration.setProperty("hibernate.dialect", instance.getProperty("db.dialect"));
+            SESSION_FACTORY = configuration.configure().buildSessionFactory();
+        }
+
         return SESSION_FACTORY;
     }
 
