d1523a33e17ca5302c9acc42245054b7d8b4df6a
poojithairosha <poojithairosha9311@gmail.com>
2023-05-20 08:55:50 +0530

    Add AdminAuthFilter

--- src/main/java/com/ceyloneseenvoy/ceyloneseenvoy/filter/AdminAuthFilter.java
+++ src/main/java/com/ceyloneseenvoy/ceyloneseenvoy/filter/AdminAuthFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-// @WebFilter(urlPatterns = {"/admin/*"})
+@WebFilter(urlPatterns = {"/admin/*"})
 public class AdminAuthFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
