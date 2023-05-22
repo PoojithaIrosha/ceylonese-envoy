505ebc48960dbb68b2bd8203f9d9bbd2b3185953
poojithairosha <poojithairosha9311@gmail.com>
2023-05-22 23:19:57 +0530

    Implement admin logout

---
+++ src/main/java/com/ceyloneseenvoy/ceyloneseenvoy/servlet/admin/AdminLogoutServlet.java
@@ -0,0 +1,18 @@
+package com.ceyloneseenvoy.ceyloneseenvoy.servlet.admin;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "AdminLogoutServlet", value = "/admin/admin-logout")
+public class AdminLogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect(req.getContextPath() + "/auth/admin/login.jsp");
+    }
+}
--- src/main/webapp/assets/js/admin.js
+++ src/main/webapp/assets/js/admin.js
@@ -72,3 +72,102 @@ function changePackageStatus(id) {
     }
     xhr.send("id=" + id);
 }
+
+$("#addAdminForm").submit((event) => {
+    event.preventDefault();
+
+    $.ajax({
+        type: "POST",
+        url: "add-admin",
+        async: true,
+        data: $("#addAdminForm").serialize(),
+        success: (response) => {
+            let json = JSON.parse(response);
+
+            if (json.status) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success',
+                    text: json.message,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location = "all-admins.jsp";
+                    }
+                })
+            }else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: json.message,
+                });
+            }
+        },
+        error: (error) => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong!',
+            });
+        }
+    });
+})
+
+function changeAdminStatus(id) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "change-admin-status", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let json = JSON.parse(this.responseText);
+            if (json.status) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success',
+                    text: json.message,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.reload();
+                    }
+                });
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: json.message,
+                });
+            }
+        }
+    }
+    xhr.send("id=" + id);
+}
+
+function deleteAdmin(id) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "delete-admin", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let json = JSON.parse(this.responseText);
+            if (json.status) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Success',
+                    text: json.message,
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.reload();
+                    }
+                });
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: json.message,
+                });
+            }
+        }
+    }
+    xhr.send("id=" + id);
+}
