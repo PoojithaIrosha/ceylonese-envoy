95c70091becf4befd412e21de7ef5e0dcd6d6052
poojithairosha <poojithairosha9311@gmail.com>
2023-05-27 21:15:22 +0530

    Updated image uploading with apache commons file upload library

--- src/main/java/com/ceyloneseenvoy/ceyloneseenvoy/servlet/admin/AddTourServlet.java
+++ src/main/java/com/ceyloneseenvoy/ceyloneseenvoy/servlet/admin/AddTourServlet.java
@@ -4,90 +4,142 @@ import com.ceyloneseenvoy.ceyloneseenvoy.model.IsActive;
 import com.ceyloneseenvoy.ceyloneseenvoy.model.TourPackage;
 import com.ceyloneseenvoy.ceyloneseenvoy.model.TourPackageImage;
 import com.ceyloneseenvoy.ceyloneseenvoy.util.HibernateUtil;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.hibernate.Session;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
 @WebServlet(name = "AddTourServlet", urlPatterns = "/admin/add-tour")
-@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 10, // 10 MB
-        maxFileSize = 1024 * 1024 * 50, // 50 MB
-        maxRequestSize = 1024 * 1024 * 100 // 100 MB
-)
 public class AddTourServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("name");
-        String price = req.getParameter("price");
-        String locations = req.getParameter("locations");
-        String overview = req.getParameter("overview");
-        String departure = req.getParameter("departure");
-        String cancellation = req.getParameter("cancellation");
-        String inclusions = req.getParameter("inclusions");
-        String exclusions = req.getParameter("exclusions");
-        String highlights = req.getParameter("highlights");
-        String additionalInfo = req.getParameter("addInfo");
+        String id = null;
+        String name = null;
+        String price = null;
+        String locations = null;
+        String overview = null;
+        String departure = null;
+        String cancellation = null;
+        String inclusions = null;
+        String exclusions = null;
+        String highlights = null;
+        String additionalInfo = null;
+        String erroMsg = "Please fill all the fields";
+        List<FileItem> images = new ArrayList<>();
 
-        String erroMsg = null;
+        // Create a DiskFileItemFactory
+        DiskFileItemFactory factory = new DiskFileItemFactory();
 
-        if (name == null || name.isEmpty()) {
-            erroMsg = "Name cannot be empty!";
-        } else if (price == null || price.isEmpty()) {
-            erroMsg = "Price cannot be empty!";
-        } else if (locations == null || locations.isEmpty()) {
-            erroMsg = "Locations cannot be empty!";
-        } else if (overview == null || overview.isEmpty()) {
-            erroMsg = "Overview cannot be empty!";
-        }
+        // Set the temporary directory for storing uploaded files
+        String tempDirPath = System.getProperty("java.io.tmpdir");
+        File tempDir = new File(tempDirPath);
+        factory.setRepository(tempDir);
 
-        if (erroMsg != null) {
-            resp.sendRedirect(req.getContextPath() + "/admin/add-tour.jsp?error=" + erroMsg);
-        } else {
+        // Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload(factory);
+
+        try {
+            // Parse the request to get the list of FileItems
+            List<FileItem> items = upload.parseRequest(req);
+
+            // Process each file item
+            for (FileItem item : items) {
+                // Check if the item is a form field or a file
+                if (item.isFormField()) {
+                    // This is a regular form field
+                    String fieldName = item.getFieldName();
+                    String fieldValue = item.getString();
+
+                    if ((fieldName.equals("id") || fieldName.equals("name") || fieldName.equals("price") || fieldName.equals("locations") || fieldName.equals("overview")) && fieldValue.isEmpty()) {
+                        resp.sendRedirect(req.getContextPath() + "/admin/add-tour.jsp?error=" + erroMsg);
+                        return;
+                    }
+
+                    switch (fieldName) {
+                        case "id":
+                            id = fieldValue;
+                            break;
+                        case "name":
+                            name = fieldValue;
+                            break;
+                        case "price":
+                            price = fieldValue;
+                            break;
+                        case "locations":
+                            locations = fieldValue;
+                            break;
+                        case "overview":
+                            overview = fieldValue;
+                            break;
+                        case "departure":
+                            departure = fieldValue;
+                            break;
+                        case "cancellation":
+                            cancellation = fieldValue;
+                            break;
+                        case "inclusions":
+                            inclusions = fieldValue;
+                            break;
+                        case "exclusions":
+                            exclusions = fieldValue;
+                            break;
+                        case "highlights":
+                            highlights = fieldValue;
+                            break;
+                        case "addInfo":
+                            additionalInfo = fieldValue;
+                            break;
+                        default:
+                            resp.sendRedirect(req.getContextPath() + "/admin/add-tour.jsp?error=" + erroMsg);
+                            return;
+                    }
+
+                } else {
+
+                    if (item.getSize() > 0) {
+                        images.add(item);
+                    }
+                }
+            }
 
             try (Session session = HibernateUtil.getSessionFactory().openSession()) {
 
                 TourPackage tourPackage = TourPackage.builder().name(name).price(Double.parseDouble(price)).locations(locations).overview(overview).departureDetails(departure).cancellationPolicy(cancellation).inclusions(inclusions).exclusions(exclusions).highlights(highlights).additionalInformation(additionalInfo).isActive(IsActive.ACTIVE).build();
 
-                Collection<Part> parts = req.getParts();
-
-                String uploadPath = System.getProperty("catalina.home") + File.separator + "ceylonese-envoy" + File.separator + "tours";
-
-                File directory = new File(uploadPath);
-                if (!directory.exists()) {
-                    directory.mkdirs();
-                }
-                System.out.println("uploadPath: " + uploadPath);
-
                 List<TourPackageImage> tourPackageImages = new ArrayList<>();
 
-                for (Part part : parts) {
-                    if (part.getName().equals("images") && part.getSize() > 0) {
-                        String filePath = uploadPath + File.separator + name + UUID.randomUUID() + ".jpg";
-                        tourPackageImages.add(TourPackageImage.builder().image(filePath).build());
+                images.forEach((item) -> {
+                    // This is an uploaded file
+                    String fileName = UUID.randomUUID() + ".jpg";
 
-                        File file = new File(filePath);
-                        try (InputStream input = part.getInputStream()) {
-                            Files.copy(input, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                    // Save the file to disk
+                    String uploadDirPath = System.getProperty("user.home") + File.separator + "/ceylonese-envoy/uploads/";
+                    File uploadDir = new File(uploadDirPath);
+                    if (!uploadDir.exists()) {
+                        uploadDir.mkdirs();
                     }
-                }
+
+                    File uploadedFile = new File(uploadDir, fileName);
+                    try {
+                        item.write(uploadedFile);
+                        tourPackageImages.add(TourPackageImage.builder().image(uploadDirPath + fileName).build());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
 
                 if (tourPackageImages.size() > 0) {
                     tourPackage.setTourPackageImages(tourPackageImages);
@@ -108,7 +160,82 @@ public class AddTourServlet extends HttpServlet {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
+
+        } catch (FileUploadException e) {
+            // Handle file upload exception
+            e.printStackTrace();
+        } catch (Exception e) {
+            // Handle other exceptions
+            e.printStackTrace();
         }
+
+        // String erroMsg = null;
+        //
+        // if (name == null || name.isEmpty()) {
+        //     erroMsg = "Name cannot be empty!";
+        // } else if (price == null || price.isEmpty()) {
+        //     erroMsg = "Price cannot be empty!";
+        // } else if (locations == null || locations.isEmpty()) {
+        //     erroMsg = "Locations cannot be empty!";
+        // } else if (overview == null || overview.isEmpty()) {
+        //     erroMsg = "Overview cannot be empty!";
+        // }
+        //
+        // if (erroMsg != null) {
+        //     resp.sendRedirect(req.getContextPath() + "/admin/add-tour.jsp?error=" + erroMsg);
+        // } else {
+        //
+        //     try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        //
+        //         TourPackage tourPackage = TourPackage.builder().name(name).price(Double.parseDouble(price)).locations(locations).overview(overview).departureDetails(departure).cancellationPolicy(cancellation).inclusions(inclusions).exclusions(exclusions).highlights(highlights).additionalInformation(additionalInfo).isActive(IsActive.ACTIVE).build();
+        //
+        //         Collection<Part> parts = req.getParts();
+        //
+        //         String uploadPath = System.getProperty("catalina.home") + File.separator + "ceylonese-envoy" + File.separator + "tours";
+        //
+        //         File directory = new File(uploadPath);
+        //         if (!directory.exists()) {
+        //             directory.mkdirs();
+        //         }
+        //         System.out.println("uploadPath: " + uploadPath);
+        //
+        //         List<TourPackageImage> tourPackageImages = new ArrayList<>();
+        //
+        //         for (Part part : parts) {
+        //             if (part.getName().equals("images") && part.getSize() > 0) {
+        //                 String filePath = uploadPath + File.separator + name + UUID.randomUUID() + ".jpg";
+        //                 tourPackageImages.add(TourPackageImage.builder().image(filePath).build());
+        //
+        //                 File file = new File(filePath);
+        //                 try (InputStream input = part.getInputStream()) {
+        //                     Files.copy(input, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        //                 } catch (Exception e) {
+        //                     e.printStackTrace();
+        //                 }
+        //             }
+        //         }
+        //
+        //         if (tourPackageImages.size() > 0) {
+        //             tourPackage.setTourPackageImages(tourPackageImages);
+        //         }
+        //
+        //         try {
+        //             session.beginTransaction();
+        //             session.save(tourPackage);
+        //             session.getTransaction().commit();
+        //
+        //             resp.sendRedirect(req.getContextPath() + "/admin/tour-packages.jsp");
+        //         } catch (Exception e) {
+        //             session.getTransaction().rollback();
+        //             req.getRequestDispatcher(req.getContextPath() + "/errors/500.jsp").forward(req, resp);
+        //             e.printStackTrace();
+        //         }
+        //
+        //     } catch (Exception e) {
+        //         e.printStackTrace();
+        //     }
+        // }
     }
 }
 
--- src/main/java/com/ceyloneseenvoy/ceyloneseenvoy/servlet/admin/UpdateTourServlet.java
+++ src/main/java/com/ceyloneseenvoy/ceyloneseenvoy/servlet/admin/UpdateTourServlet.java
@@ -3,86 +3,119 @@ package com.ceyloneseenvoy.ceyloneseenvoy.servlet.admin;
 import com.ceyloneseenvoy.ceyloneseenvoy.model.TourPackage;
 import com.ceyloneseenvoy.ceyloneseenvoy.model.TourPackageImage;
 import com.ceyloneseenvoy.ceyloneseenvoy.util.HibernateUtil;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.hibernate.Session;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
 @WebServlet(name = "UpdateTourServlet", urlPatterns = "/admin/update-tour")
-@MultipartConfig(
-        fileSizeThreshold = 1024 * 1024 * 10, // 10 MB
-        maxFileSize = 1024 * 1024 * 50, // 50 MB
-        maxRequestSize = 1024 * 1024 * 100 // 100 MB
-)
 public class UpdateTourServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String id = req.getParameter("id");
-        String name = req.getParameter("name");
-        String price = req.getParameter("price");
-        String locations = req.getParameter("locations");
-        String overview = req.getParameter("overview");
-        String departure = req.getParameter("departure");
-        String cancellation = req.getParameter("cancellation");
-        String inclusions = req.getParameter("inclusions");
-        String exclusions = req.getParameter("exclusions");
-        String highlights = req.getParameter("highlights");
-        String additionalInfo = req.getParameter("addInfo");
 
-        String erroMsg = null;
+        String id = null;
+        String name = null;
+        String price = null;
+        String locations = null;
+        String overview = null;
+        String departure = null;
+        String cancellation = null;
+        String inclusions = null;
+        String exclusions = null;
+        String highlights = null;
+        String additionalInfo = null;
+        String erroMsg = "Please fill all the fields";
+        List<FileItem> images = new ArrayList<>();
 
-        if (name == null || name.isEmpty()) {
-            erroMsg = "Name cannot be empty!";
-        }else if(price == null || price.isEmpty()) {
-            erroMsg = "Price cannot be empty!";
-        }else if(locations == null || locations.isEmpty()) {
-            erroMsg = "Locations cannot be empty!";
-        }else if(overview == null || overview.isEmpty()) {
-            erroMsg = "Overview cannot be empty!";
-        }
 
-        if(departure == null || departure.isEmpty()) {
-            departure = null;
-        }
+        // Create a DiskFileItemFactory
+        DiskFileItemFactory factory = new DiskFileItemFactory();
 
-        if(cancellation == null || cancellation.isEmpty()) {
-            cancellation = null;
-        }
+        // Set the temporary directory for storing uploaded files
+        String tempDirPath = System.getProperty("java.io.tmpdir");
+        File tempDir = new File(tempDirPath);
+        factory.setRepository(tempDir);
 
-        if(inclusions == null || inclusions.isEmpty()) {
-            inclusions = null;
-        }
+        // Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload(factory);
 
-        if(exclusions == null || exclusions.isEmpty()) {
-            exclusions = null;
-        }
+        try {
+            // Parse the request to get the list of FileItems
+            List<FileItem> items = upload.parseRequest(req);
 
-        if(highlights == null || highlights.isEmpty()) {
-            highlights = null;
-        }
+            // Process each file item
+            for (FileItem item : items) {
+                // Check if the item is a form field or a file
+                if (item.isFormField()) {
+                    // This is a regular form field
+                    String fieldName = item.getFieldName();
+                    String fieldValue = item.getString();
 
-        if(additionalInfo == null || additionalInfo.isEmpty()) {
-            additionalInfo = null;
-        }
+                    if ((fieldName.equals("id") || fieldName.equals("name") || fieldName.equals("price") || fieldName.equals("locations") || fieldName.equals("overview")) && fieldValue.isEmpty()) {
+                        resp.sendRedirect(req.getContextPath() + "/admin/edit-tour.jsp?package=" + id + "&error=" + erroMsg);
+                        return;
+                    }
 
-        if(erroMsg != null) {
-            resp.sendRedirect(req.getContextPath() + "/admin/edit-tour.jsp?package=" + id + "&error=" + erroMsg);
-        }else {
+                    switch (fieldName) {
+                        case "id":
+                            id = fieldValue;
+                            break;
+                        case "name":
+                            name = fieldValue;
+                            break;
+                        case "price":
+                            price = fieldValue;
+                            break;
+                        case "locations":
+                            locations = fieldValue;
+                            break;
+                        case "overview":
+                            overview = fieldValue;
+                            break;
+                        case "departure":
+                            departure = fieldValue;
+                            break;
+                        case "cancellation":
+                            cancellation = fieldValue;
+                            break;
+                        case "inclusions":
+                            inclusions = fieldValue;
+                            break;
+                        case "exclusions":
+                            exclusions = fieldValue;
+                            break;
+                        case "highlights":
+                            highlights = fieldValue;
+                            break;
+                        case "addInfo":
+                            additionalInfo = fieldValue;
+                            break;
+                        default:
+                            resp.sendRedirect(req.getContextPath() + "/admin/edit-tour.jsp?package=" + id + "&error=" + erroMsg);
+                            return;
+                    }
+
+                } else {
+
+                    if (item.getSize() > 0) {
+                        images.add(item);
+                    }
+                }
+            }
+
             try (Session session = HibernateUtil.getSessionFactory().openSession()) {
                 TourPackage tourPackage = session.get(TourPackage.class, Long.parseLong(id));
                 tourPackage.setName(name);
@@ -96,44 +129,40 @@ public class UpdateTourServlet extends HttpServlet {
                 tourPackage.setHighlights(highlights);
                 tourPackage.setAdditionalInformation(additionalInfo);
 
-                Collection<Part> parts = req.getParts();
                 List<TourPackageImage> tourPackageImages = new ArrayList<>();
+                images.forEach((item) -> {
+                    // This is an uploaded file
+                    String fileName = UUID.randomUUID() + ".jpg";
 
-                String uploadPath = System.getProperty("catalina.home") + File.separator + "ceylonese-envoy" + File.separator + "tours";
-
-                File directory = new File(uploadPath);
-                if (!directory.exists()) {
-                    directory.mkdirs();
-                }
-
-                for (Part part : parts) {
-                    if (part.getName().equals("images") && part.getSize() > 0) {
-                        String filePath = uploadPath + File.separator + name + UUID.randomUUID() + ".jpg";
-                        tourPackageImages.add(TourPackageImage.builder().image(filePath).build());
-
-                        File file = new File(filePath);
-                        try (InputStream input = part.getInputStream()) {
-                            Files.copy(input, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                    // Save the file to disk
+                    String uploadDirPath = System.getProperty("user.home") + File.separator + "/ceylonese-envoy/uploads/";
+                    File uploadDir = new File(uploadDirPath);
+                    if (!uploadDir.exists()) {
+                        uploadDir.mkdirs();
                     }
-                }
+
+                    File uploadedFile = new File(uploadDir, fileName);
+                    try {
+                        item.write(uploadedFile);
+                        tourPackageImages.add(TourPackageImage.builder().image(uploadDirPath + fileName).build());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
 
                 try {
                     session.beginTransaction();
 
-                    if (!tourPackageImages.isEmpty()) {
-                        for (TourPackageImage tpi : tourPackage.getTourPackageImages()) {
-                            File file = new File(tpi.getImage());
-                            if (file.exists()) {
-                                file.delete();
-                            }
-
-                            session.createQuery("delete from TourPackageImage where id = :id")
-                                    .setParameter("id", tpi.getId())
-                                    .executeUpdate();
+                    for (TourPackageImage tpi : tourPackage.getTourPackageImages()) {
+                        File file = new File(tpi.getImage());
+                        if (file.exists()) {
+                            file.delete();
                         }
+
+                        session.createQuery("delete from TourPackageImage where id = :id").setParameter("id", tpi.getId()).executeUpdate();
+                    }
+
+                    if (tourPackageImages.size() > 0) {
                         tourPackage.setTourPackageImages(tourPackageImages);
                     }
 
@@ -146,7 +175,15 @@ public class UpdateTourServlet extends HttpServlet {
                     req.getRequestDispatcher(req.getContextPath() + "/errors/500.jsp").forward(req, resp);
                     e.printStackTrace();
                 }
+
             }
+
+        } catch (FileUploadException e) {
+            // Handle file upload exception
+            e.printStackTrace();
+        } catch (Exception e) {
+            // Handle other exceptions
+            e.printStackTrace();
         }
     }
 }
